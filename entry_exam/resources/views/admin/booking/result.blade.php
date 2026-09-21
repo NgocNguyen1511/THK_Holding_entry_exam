02200a338173427da134167cfa1dc82005c58f77
ngocnguyen <nguyenphucngoc1511@gmail.com>
2026-09-21 18:34:56 +0700

    fix: booking result responsive

--- entry_exam/resources/views/admin/booking/result.blade.php
+++ entry_exam/resources/views/admin/booking/result.blade.php
@@ -5,58 +5,60 @@
         <div class="search-result">
             <h3 class="search-result-title">検索結果</h3>
             @if (!empty($bookings))
-                <table class="shopsearchlist_table">
-                    <tbody>
-                        <tr>
-                            <td nowrap="" id="customer_name">
-                                顧客名
-                            </td>
-                            <td nowrap="" id="customer_contact">
-                                顧客連絡先
-                            </td>
-                            <td nowrap="" id="hotel_name">
-                                宿泊先ホテル
-                            </td>
-                            <td nowrap="" id="checkin_time">
-                                チェックイン日時
-                            </td>
-                            <td nowrap="" id="checkout_time">
-                                チェックアウト日時
-                            </td>
-                            <td nowrap="" id="created_at">
-                                予約日時
-                            </td>
-                            <td nowrap="" id="updated_at">
-                                情報更新日時
-                            </td>
-                        </tr>
-                        @foreach ($bookings as $booking)
-                            <tr style="background-color:#BDF1FF">
-                                <td>
-                                    {{ $booking['customer_name'] }}
+                <div style="overflow-x: auto; max-width: 100%;">
+                    <table class="shopsearchlist_table">
+                        <tbody>
+                            <tr>
+                                <td nowrap="" id="customer_name">
+                                    顧客名
                                 </td>
-                                <td>
-                                    {{ $booking['customer_contact'] }}
+                                <td nowrap="" id="customer_contact">
+                                    顧客連絡先
                                 </td>
-                                <td>
-                                    {{ $booking['hotel']['hotel_name'] ?? '-' }}
+                                <td nowrap="" id="hotel_name">
+                                    宿泊先ホテル
                                 </td>
-                                <td nowrap="">
-                                    {{ $booking['checkin_time'] ? (string) $booking['checkin_time'] : '-' }}
+                                <td nowrap="" id="checkin_time">
+                                    チェックイン日時
                                 </td>
-                                <td nowrap="">
-                                    {{ $booking['checkout_time'] ? (string) $booking['checkout_time'] : '-' }}
+                                <td nowrap="" id="checkout_time">
+                                    チェックアウト日時
                                 </td>
-                                <td nowrap="">
-                                    {{ $booking['created_at'] ? (string) $booking['created_at'] : '-' }}
+                                <td nowrap="" id="created_at">
+                                    予約日時
                                 </td>
-                                <td nowrap="">
-                                    {{ $booking['updated_at'] ? (string) $booking['updated_at'] : '-' }}
+                                <td nowrap="" id="updated_at">
+                                    情報更新日時
                                 </td>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            @foreach ($bookings as $booking)
+                                <tr style="background-color:#BDF1FF">
+                                    <td style="white-space: nowrap;">
+                                        {{ $booking['customer_name'] }}
+                                    </td>
+                                    <td style="white-space: nowrap;">
+                                        {{ $booking['customer_contact'] }}
+                                    </td>
+                                    <td style="white-space: nowrap;">
+                                        {{ $booking['hotel']['hotel_name'] ?? '-' }}
+                                    </td>
+                                    <td nowrap="">
+                                        {{ $booking['checkin_time'] ? (string) $booking['checkin_time'] : '-' }}
+                                    </td>
+                                    <td nowrap="">
+                                        {{ $booking['checkout_time'] ? (string) $booking['checkout_time'] : '-' }}
+                                    </td>
+                                    <td nowrap="">
+                                        {{ $booking['created_at'] ? (string) $booking['created_at'] : '-' }}
+                                    </td>
+                                    <td nowrap="">
+                                        {{ $booking['updated_at'] ? (string) $booking['updated_at'] : '-' }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             @else
                 <p>検索結果がありません</p>
             @endif
